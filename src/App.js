13885d73cb23dec64c47bd564c9aadc43cbbdd08
rirefat <rirefat.official@gmail.com>
2022-03-25 22:04:34 +0600

    Done Navbar Part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
-      <h1 className='text-info'>Hello</h1>
+      <Header></Header>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import logo from '../../images/mowat-logo.png'
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            
+            <>
+            <Navbar bg="light" variant="light" className='navbar-main'>
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img src={logo} alt=""/>
+                    </Navbar.Brand>
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#products">Products</Nav.Link>
+                        <Nav.Link href="#cart">Cart</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            </>
         </div>
     );
 };
